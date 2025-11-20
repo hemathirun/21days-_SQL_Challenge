cd147a222577a197baa32d13549f7a1e6254aa0b
hemathirun <hema.thirun@gmail.com>
2025-11-19 22:04:05 -0500

    Update Hospital _1Question.sql
    Day 2 Challenge

--- Hospital _1Question.sql
+++ Hospital _1Question.sql
@@ -9,7 +9,18 @@
 SELECT DISTINCT service
 FROM services_weekly
 order by service;
+--Daily Challenge DAY 2
 
+--Question : 
+--Find all patients admitted to 'Surgery' service with a satisfaction score below 70?
+SELECT 
+    patient_id,
+    name,
+    age,
+    satisfaction AS satisfaction_score
+FROM patients
+WHERE service = 'surgery'
+  AND satisfaction < 70;
 
 
 
