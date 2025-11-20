99bc244e614e22a704417cea2d4a06201d819524
hemathirun <hema.thirun@gmail.com>
2025-11-19 21:39:27 -0500

    Create Hospital _1Question.sql
    Day 1 Challenge

---
+++ Hospital _1Question.sql
@@ -0,0 +1,22 @@
+
+
+
+--DAY 1 Challengs 
+
+
+--Question: List all unique hospital services available in the hospital?
+
+SELECT DISTINCT service
+FROM services_weekly
+order by service;
+
+
+
+
+
+
+
+
+
+
+
