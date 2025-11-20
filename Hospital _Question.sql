788b5f9c433d588ede793c443b160f1c03ef0ffc
hemathirun <hema.thirun@gmail.com>
2025-11-20 14:45:43 -0500

    Update Hospital _Question.sql
    Day 11 Challenge

--- Hospital _Question.sql
+++ Hospital _Question.sql
@@ -141,6 +141,21 @@ FROM services_weekly
 GROUP BY service
 ORDER BY avg_satisfaction DESC;
 
+--Daily Challenge Day 11
+--Question: Find all unique combinations of service and event type from the services_weekly 
+--table where events are not null or none, along with the count of occurrences for 
+--each combination. Order by count descending?
+
+SELECT
+    service,
+    event,
+    COUNT(*) AS total_occurrences
+FROM services_weekly
+WHERE event IS NOT NULL
+  AND event <> 'none'
+GROUP BY service, event
+ORDER BY total_occurrences DESC;
+
 
 
 
