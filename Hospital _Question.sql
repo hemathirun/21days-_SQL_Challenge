ab185bbbfa5b7ca2da739c1d7e5ff64f64e0ea66
hemathirun <hema.thirun@gmail.com>
2025-11-25 22:38:07 -0500

    Update Hospital _Question.sql
    Day 19 Challenge

--- Hospital _Question.sql
+++ Hospital _Question.sql
@@ -376,5 +376,32 @@ WHERE LOWER(service) IN ('surgery', 'emergency')
 ORDER BY type, service, full_name;
 
 
+----Daily Challenge Day 19
+
+--For each service, rank the weeks by patient satisfaction score (highest first). 
+--Show service, week, patient_satisfaction, patients_admitted, and the rank. 
+--Include only the top 3 weeks per service.
+
+WITH satisfaction_rank AS (
+    SELECT
+        service,
+        week,
+        patient_satisfaction,
+        patients_admitted,
+        RANK() OVER (
+            PARTITION BY service
+            ORDER BY patient_satisfaction DESC
+        ) AS sat_rank
+    FROM services_weekly
+)
+SELECT
+    service,
+    week,
+    patient_satisfaction,
+    patients_admitted,
+    sat_rank AS rank
+FROM satisfaction_rank
+WHERE sat_rank <= 3
+ORDER BY service, rank, week;
 
 
